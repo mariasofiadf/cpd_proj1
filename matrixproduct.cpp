5005f8d2e585ca83844ebafb6ad78f413ec717e5
ritaramada <anaritaramada@gmail.com>
2022-03-15 17:45:20 +0000

    Block multiplication

--- matrixproduct.cpp
+++ matrixproduct.cpp
@@ -135,7 +135,79 @@ void OnMultLine(int m_ar, int m_br)
 // add code here for block x block matriz multiplication
 void OnMultBlock(int m_ar, int m_br, int bkSize)
 {
-    
+    SYSTEMTIME Time1, Time2;
+	
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+	
+
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+
+
+    Time1 = clock();
+// for (int ii = 0;  ii < m_ar; ii++){
+// 	for (int jj = 0;  jj < m_br; jj++){
+// 		for (int kk = 0;  kk < m_br; kk++){
+// 			for(i=ii*bkSize; i< (ii+1)*bkSize; i++){
+// 				for( k=kk; k<(kk+1)*bkSize; k++){	
+// 					for( j=jj*bkSize; j<(jj+1)*bkSize; j++){	
+// 						phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j];
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// }
+
+
+	for(int ii = 0; ii < m_ar; ii += bkSize){
+		for(int jj=0;jj<m_ar;jj+= bkSize){
+			for(int kk=0;kk<m_ar;kk+= bkSize){
+				for(int i=0;i<bkSize;i++){
+					for(int k = 0; k<bkSize; k++){
+						for(int j = 0; j<bkSize; j++){
+						
+							phc[(ii+i)*m_ar+ (jj+j)]  += pha[(ii+i)*m_ar+(kk+k)]* phb[(kk+k)*m_ar+(jj+j)];
+						}
+					}
+				}
+			}
+		}
+	}
+
+    Time2 = clock();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
+	
+
     
 }
 
